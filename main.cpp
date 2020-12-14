670fa2658d2a0bab24a54e276120a3cd7156bcb0
myk1515 <962989520@qq.com>
2020-12-14 18:37:03 +0800

    feat:draw workpiece with material
    by:maoyankai

--- main.cpp
+++ main.cpp
@@ -176,6 +176,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
 
 	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "CG_project", NULL, NULL);
 	if (window == NULL) {
@@ -215,7 +216,7 @@ int main() {
 
 
 
-
+	glEnable(GL_MULTISAMPLE);
 	glEnable(GL_DEPTH_TEST);
 
 	while (!glfwWindowShouldClose(window)) {
